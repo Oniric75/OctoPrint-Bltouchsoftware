c4a1ed78c8089d68ea77d63b2e7c67206b947774
oniric <4118495+Oniric75@users.noreply.github.com>
2019-03-09 20:57:32 +0100

    improve regex m114
    Signed-off-by: oniric <4118495+Oniric75@users.noreply.github.com>

--- octoprint_BLTouchSoftware/__init__.py
+++ octoprint_BLTouchSoftware/__init__.py
@@ -130,7 +130,7 @@ class BltouchsoftwarePlugin(octoprint.plugin.StartupPlugin,
 	def read_m114(self, comm, line, *args, **kwargs):
 		if BedLeveling.active is True:
 			# self._logger.info("===comm:%s, line:%s===" % (comm, line))
-			m114 = re.match(r"ok X:(\d+\.\d+) Y:(\d+\.\d+) Z:(\d+\.\d+).*", line, re.IGNORECASE)
+			m114 = re.match(r"ok\s*X:\s*(\d+\.\d+)\s*Y:\s*(\d+\.\d+)\s*Z:\s*(\d+\.\d+).*", line, re.IGNORECASE)
 			if m114:
 				self._logger.info("M114 result: X:%s|Y:%s|Z:%s" % (m114.group(1), m114.group(2), m114.group(3)))
 				BedLeveling.set_current_pos(float(m114.group(1)), float(m114.group(2)), float(m114.group(3)))
