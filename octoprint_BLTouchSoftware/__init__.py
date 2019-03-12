120914f33be9d853f0a193164c14e06bd4ba58a1
oniric <4118495+Oniric75@users.noreply.github.com>
2019-03-12 20:19:20 +0100

    minor bug
    Signed-off-by: oniric <4118495+Oniric75@users.noreply.github.com>

--- octoprint_BLTouchSoftware/__init__.py
+++ octoprint_BLTouchSoftware/__init__.py
@@ -118,8 +118,8 @@ class BltouchsoftwarePlugin(octoprint.plugin.StartupPlugin,
 
 	def buildreplacement(self, g1):
 		replstr = r"\1"
-		off_z = BedLeveling.get_z(BedLeveling.current_position[BedLeveling.X_AXIS],
-								  BedLeveling.current_position[BedLeveling.Y_AXIS])
+		off_z = float(BedLeveling.get_z(BedLeveling.current_position[BedLeveling.X_AXIS],
+										BedLeveling.current_position[BedLeveling.Y_AXIS]))
 		pz = BedLeveling.current_position[BedLeveling.Z_AXIS] + off_z
 		if g1.group(2) is not None:
 			replstr += r" %s" % g1.group(2)
@@ -128,7 +128,7 @@ class BltouchsoftwarePlugin(octoprint.plugin.StartupPlugin,
 		if g1.group(4) is not None:
 			replstr += r" Y:%s" % g1.group(4)
 		if g1.group(5) is not None:
-			replstr += r" Z:%0.3f" % float(g1.group(5) + off_z)
+			replstr += r" Z:%0.3f" % (float(g1.group(5)) + off_z)
 		else:
 			replstr += r"Z:%0.3f" % pz
 		if g1.group(6) is not None:
