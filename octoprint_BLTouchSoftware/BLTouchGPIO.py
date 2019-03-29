be81e92d9c1c88c24c566d46d070edaca74b503f
oniric <4118495+Oniric75@users.noreply.github.com>
2019-03-29 16:19:50 +0100

    send back Zmin to printer
    Signed-off-by: oniric <4118495+Oniric75@users.noreply.github.com>

--- octoprint_BLTouchSoftware/BLTouchGPIO.py
+++ octoprint_BLTouchSoftware/BLTouchGPIO.py
@@ -24,15 +24,13 @@ class BLTouchGPIO:
 
 		GPIO.cleanup()
 		GPIO.setmode(GPIO.BOARD)
-		GPIO.remove_event_detect(self.GPIO_BLTouch_Zmin)
-		GPIO.remove_event_detect(self.GPIO_Switch_Zmin)
 
 		# callback_bltouch_zmin when bltouch touch the bed
 		GPIO.setup(self.GPIO_BLTouch_Zmin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.add_event_detect(self.GPIO_BLTouch_Zmin, GPIO.RISING, callback=self.callback_bltouch_zmin, bouncetime=200)
 
 		# callback_bltouch_zmin when the zswitch is trigger
-		GPIO.setup(self.GPIO_Switch_Zmin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		GPIO.setup(self.GPIO_Switch_Zmin, GPIO.IN)
 		GPIO.add_event_detect(self.GPIO_Switch_Zmin, GPIO.RISING, callback=self.callback_switch_zmin, bouncetime=200)
 
 		# send zmin to printer
